68274438f47aebaf8a41c190f4b0cf6644083888
Mauricio Henao <atomauro@cosmos-2.local>
2022-06-10 16:31:11 -0500

    .

--- src/components/Contract/Contract.jsx
+++ src/components/Contract/Contract.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable */
+import React, { useEffect } from "react";
 import { Form, notification } from "antd";
 import { useMemo, useState } from "react";
 import Address from "components/Address/Address";
@@ -9,15 +10,108 @@ import ContractResolver from "./ContractResolver";
 
 // Chakra imports
 import {
-  Flex,
+  Flex, Text
 } from "@chakra-ui/react";
 // Custom components
 import Card from "../../components/Card/Card.js";
+import Blockie from "../Blockie";
 
 export default function Contract() {
-  const { Moralis, chainId } = useMoralis();
+  const { Moralis, chainId, isAuthenticated, account } = useMoralis();
   const [responses, setResponses] = useState({});
   const [contract, setContract] = useState();
+
+  const [address, setAddress] = useState();
+
+  const styles = {
+    title: {
+      fontSize: "30px",
+      fontWeight: "600",
+    },
+    header: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap: "5px",
+    },
+    card: {
+      boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+      border: "1px solid #e7eaf3",
+      borderRadius: "1rem",
+      width: "450px",
+      fontSize: "16px",
+      fontWeight: "500",
+    },
+  };
+
+  useEffect(() => {
+    setAddress((isAuthenticated && account));
+  }, [account, isAuthenticated]);
+
+  if (!address) {
+    return (
+      <Flex
+        direction="column"
+        pt={{ base: "120px", md: "75px" }}
+        alignContent="center"
+        alignItems="center"
+      ><Card
+        style={styles.card}
+        title={
+          <div style={styles.header}>
+            <Blockie scale={5} avatar currentWallet style />
+            <Address size="6" copyable />
+          </div>
+        }
+      >
+          <div
+            style={{
+              width: "auto",
+              height: "300px",
+              justifyContent: 'center',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <Text>Please connect Wallet</Text>
+          </div>
+
+        </Card>
+      </Flex>
+    );
+  }
+  else {
+    return (
+      <Flex
+        direction="column"
+        pt={{ base: "120px", md: "75px" }}
+        alignContent="center"
+        alignItems="center"
+      ><Card
+        style={styles.card}
+        title={
+          <div style={styles.header}>
+            <Blockie scale={5} avatar currentWallet style />
+            <Address size="6" copyable />
+          </div>
+        }
+      >
+          <div
+            style={{
+              width: "auto",
+              height: "300px",
+              justifyContent: 'center',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <Text>Coming Soon</Text>
+          </div>
+
+        </Card>
+      </Flex>
+    );
+  }
 
   /**Moralis Live query for displaying contract's events*/
   const { data } = useMoralisQuery("Events", (query) => query, [], {
--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -1,6 +1,15 @@
+/* eslint-disable */
+
 import { Card, Timeline, Typography } from "antd";
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
+import Blockie from "../components/Blockie";
+import Address from "components/Address/Address";
+
+// Chakra imports
+import {
+  Flex
+} from "@chakra-ui/react";
 
 const { Text } = Typography;
 
@@ -20,15 +29,89 @@ const styles = {
   timeline: {
     marginBottom: "-45px",
   },
+  cardoffline: {
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    borderRadius: "1rem",
+    width: "450px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
 };
 
 export default function QuickStart({ isServerInfo }) {
-  const { Moralis } = useMoralis();
+  const { Moralis, isAuthenticated, account } = useMoralis();
 
   const isInchDex = useMemo(
     () => (Moralis.Plugins?.oneInch ? true : false),
     [Moralis.Plugins?.oneInch],
   );
+
+  const [address, setAddress] = useState();
+
+  useEffect(() => {
+    setAddress((isAuthenticated && account));
+  }, [account, isAuthenticated]);
+
+  if (!address) {
+    return (
+      <Flex
+        direction="column"
+        pt={{ base: "120px", md: "75px" }}
+        alignContent="center"
+        alignItems="center"
+      ><Card
+        style={styles.cardoffline}
+
+      >
+          <div
+            style={{
+              width: "auto",
+              height: "300px",
+              justifyContent: 'center',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <Text>Please connect Wallet</Text>
+          </div>
+
+        </Card>
+      </Flex>
+    );
+  }
+  else {
+    return (
+      <Flex
+        direction="column"
+        pt={{ base: "120px", md: "75px" }}
+        alignContent="center"
+        alignItems="center"
+      ><Card
+        style={styles.card}
+        title={
+          <div style={styles.header}>
+            <Blockie scale={5} avatar currentWallet style />
+            <Address size="6" copyable />
+          </div>
+        }
+      >
+          <div
+            style={{
+              width: "auto",
+              height: "300px",
+              justifyContent: 'center',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <Text>Coming Soon</Text>
+          </div>
+
+        </Card>
+      </Flex>
+    );
+  }
 
   return (
     <div style={{ display: "flex", gap: "10px" }}>

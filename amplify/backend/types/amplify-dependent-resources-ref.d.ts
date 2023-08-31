59a4b9a46eab70330be500ddfc538907788dc74c
Mauricio Henao <atomauro@cosmos-2.local>
2023-08-31 18:05:38 -0500

    ahora si

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,21 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
-  auth: {
-    OHMIO: {
-      IdentityPoolId: "string";
-      IdentityPoolName: "string";
-      UserPoolId: "string";
-      UserPoolArn: "string";
-      UserPoolName: "string";
-      AppClientIDWeb: "string";
-      AppClientID: "string";
-      CreatedSNSRole: "string";
-    };
-  };
-  api: {
-    ohmio: {
-      GraphQLAPIKeyOutput: "string";
-      GraphQLAPIIdOutput: "string";
-      GraphQLAPIEndpointOutput: "string";
-    };
-  };
-};
+    "auth": {
+        "OHMIO": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "api": {
+        "ohmio": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
